2312b19f76b4089dd8b75ae098a47f06798b6636
Jeremiah Bargar <jeremy@formigaconsulting.com>
2020-11-07 07:39:28 -0500

    Updates

---
+++ precache-manifest.5d09cf331ef46c9f9fc76e10727e345c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05e9d856f18319b93a34ccbe63ef618",
+    "url": "/rock-scissors-paper/index.html"
+  },
+  {
+    "revision": "0e96ff0261958068197b",
+    "url": "/rock-scissors-paper/static/css/main.460b928f.chunk.css"
+  },
+  {
+    "revision": "72ee7197cbe7ace7fe72",
+    "url": "/rock-scissors-paper/static/js/2.5b38c66d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-scissors-paper/static/js/2.5b38c66d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e96ff0261958068197b",
+    "url": "/rock-scissors-paper/static/js/main.ebd9286d.chunk.js"
+  },
+  {
+    "revision": "909cceb793909a75969b",
+    "url": "/rock-scissors-paper/static/js/runtime-main.ccfad6a7.js"
+  }
+]);
